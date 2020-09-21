6897ad8811d226ddb6faec004cd7c39d64f11c5e
Hajar Helal <hajerhelal44@gmail.com>
2020-09-21 23:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function myAnimal() {
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' 
+  // P.S.: You can't just hard-code 'cat'
   let animal = "cat"
   return animal
 }
